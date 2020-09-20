56ea3de1d30b5cb3c7ade3f1ae293f3920101a02
Lucas <estebeweb@gmail.com>
2020-09-20 17:31:21 +0200

    :sparkles: Fixtures added. Updated README

---
+++ README.md
@@ -0,0 +1,6 @@
+# CRM API
+This is an API for a CRM made using Symfony and the API Platform frameworks.
+
+
+
+:Work in progress:
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,37 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Customer;
+use App\Entity\Invoice;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('en_US');
 
+        for ($c = 0; $c < 30; $c++){
+            $customer = new Customer();
+            $customer->setFirstName($faker->firstName);
+            $customer->setLastName($faker->lastName);
+            $customer->setCompany($faker->company);
+            $customer->setEmail($faker->email);
+
+            $manager->persist($customer);
+
+            for ($i = 0; $i < mt_rand(3,10); $i++){
+                $invoice = new Invoice();
+                $invoice->setAmount($faker->randomFloat(2,250,5000))
+                    ->setSentAt($faker->dateTimeBetween('-6 months'))
+                    ->setStatus($faker->randomElement(['SENT','PAID','CANCELLED']))
+                    ->setCustomer($customer);
+
+                $manager->persist($invoice);
+            }
+        }
         $manager->flush();
     }
 }
